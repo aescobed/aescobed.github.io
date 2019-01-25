242e190530c6fff78a51e4a21fc0455bc4ec1ae7
aescobed <43122995+aescobed@users.noreply.github.com>
2019-01-24 23:38:39 -0800

    newweb1

---
+++ scriptTwo.js
@@ -0,0 +1,13 @@
+//script2
+< !DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+        <title>Website under construction :)ss</title>
+</head>
+
+    <body>
+        <h1>Website under construction :)</h1>
+    </body>
+console.log('Website under construction.');
+</html>
